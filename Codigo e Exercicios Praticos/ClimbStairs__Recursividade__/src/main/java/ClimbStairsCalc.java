59f66de4a7b8b74d899e5f7d974205ed9666aaf4
Igor Augusto <igor-augustofc@hotmail.com>
2025-09-16 23:55:36 -0300

    Subindo exercicio completo Climb Stairs

--- Codigo e Exercicios Praticos/ClimbStairs__Recursividade__/src/main/java/ClimbStairsCalc.java
+++ Codigo e Exercicios Praticos/ClimbStairs__Recursividade__/src/main/java/ClimbStairsCalc.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class ClimbStairsCalc {
+
+    public int CalcularPassos(int n) {
+        if (n <= 1)
+            return 1;
+
+        return CalcularPassos(n - 1) + CalcularPassos(n - 2);
+
+
+    }
 }
--- Codigo e Exercicios Praticos/ClimbStairs__Recursividade__/src/main/java/ClimbStairsMain.java
+++ Codigo e Exercicios Praticos/ClimbStairs__Recursividade__/src/main/java/ClimbStairsMain.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ClimbStairsMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        ClimbStairsCalc climbStairsCalc = new ClimbStairsCalc();
+
+        System.out.printf("Digite a quantidade de degraus que deseja calcular: ");
+        n = sc.nextInt();
+        System.out.println("Se o número de degraus for " + n +" Há " + climbStairsCalc.CalcularPassos(n) +" maneiras distintas de subir até o topo");
+
+
+
+    }
 }
